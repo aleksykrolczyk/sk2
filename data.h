b7f19c1df91ba8aac4256bec5cc04f5e3eb71d70
Dominos1234 <52161775+Dominos1234@users.noreply.github.com>
2020-01-18 19:09:33 +0100

    Update data.h

--- data.h
+++ data.h
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <iostream>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <winsock.h>
 
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 
 #include <string.h>
@@ -28,8 +24,6 @@ const char SEND_TEXT = 'e';
 const char GET_FILE_NAMES = 'f';
 const char UPDATE_FILE = 'g';
 
-
-// Not a good practice but whatever
 
 using namespace std;
 
@@ -52,7 +46,7 @@ string read_message(int cli_fd){
     string temp_msg = "";
     char symbol = 0;
     while(symbol != END_SYMBOL){
-        read(cli_fd, &symbol, sizeof(char));
+        recv(cli_fd, &symbol, sizeof(char),0);
         temp_msg += symbol;
     }
     return temp_msg;
@@ -66,13 +60,13 @@ void send_message(int fd, char comm, string content){
     
     strcpy(msg, content.c_str());
     
-    write(fd, &comm, sizeof(char));
+    send(fd, &comm, sizeof(char),0);
     while(symbols_sent != size){
         symbols_sent += write(fd, msg, size);
         cout << symbols_sent << " / " << size;
     }
     
-    write(fd, &END_SYMBOL, sizeof(END_SYMBOL));
+    send(fd, &END_SYMBOL, sizeof(END_SYMBOL),0);
 
     delete[] msg;
     cout << "... DONE" << endl;
